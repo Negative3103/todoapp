ac9d62dbed5e1ba8519323d18bf5629afff882fe
Khasan Davronbekov <xasand3103@gmail.com>
2025-01-17 12:25:00 +0500

    get lists and list by id

--- todo.go
+++ todo.go
@@ -3,9 +3,9 @@ package todoapp
 import "errors"
 
 type TodoList struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:title binding:"required"`
+	Description string `json:"description" db:"description"`
 }
 
 type UsersList struct {
